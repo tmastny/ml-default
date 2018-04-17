5b5683fb7e47dc6ae5edd4d375024868afbdb882
Timothy Mastny <tim.mastny@gmail.com>
2018-04-17 11:44:43 -0500

    xls to csv

---
+++ R/cleaner.R
@@ -0,0 +1,4 @@
+library(here)
+library(tidyverse)
+  
+d <- read_csv(here("data", "defaults.csv"))
---
+++ R/to_csv.R
@@ -0,0 +1,8 @@
+library(here)
+library(tidyverse)
+  
+d <- readxl::read_xls(here("data", "default of credit card clients.xls"), skip = 1)
+
+d %>% 
+  rename(default = `default payment next month`) %>%
+  write_csv(here("data", "defaults.csv"))
